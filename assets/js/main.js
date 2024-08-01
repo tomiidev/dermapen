d9dad02f76e8c29ebbf92d8a0eb9e6f0339cc538
tomiidev <tomasruglio21@gmail.com>
2024-08-01 16:08:14 -0300

    vf

--- assets/js/main.js
+++ assets/js/main.js
@@ -228,3 +228,31 @@
   document.addEventListener('scroll', navmenuScrollspy);
 
 })();
+
+
+
+// Obtener el modal
+var modal = document.getElementById("myModal");
+
+// Obtener el botón que abre el modal
+var btn = document.getElementById("openModal");
+
+// Obtener el elemento <span> que cierra el modal
+var span = document.getElementsByClassName("close")[0];
+
+// Cuando el usuario hace clic en el botón, abre el modal
+btn.onclick = function() {
+    modal.style.display = "flex";
+}
+
+// Cuando el usuario hace clic en <span> (x), cierra el modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Cuando el usuario hace clic en cualquier lugar fuera del modal, lo cierra
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
